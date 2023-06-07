038855507e21d405a058b82038fa96ab2c694dc4
qingyao <qingy.huang@gmail.com>
2023-06-07 21:38:58 +0200

    keep session allow expose header to keep and send file name

--- computeAbundanceApp/__init__.py
+++ computeAbundanceApp/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+
+cors = CORS(app, supports_credentials=True)
+cors.init_app(app, resource={r"/api/*": {"expose_headers": "*"}})
 
 @app.route('/')
 def index():
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from computeAbundanceApp import app
-from flask import request, send_file, session
+from flask import request, send_file, session, jsonify
 from werkzeug.utils import secure_filename
 import os, uuid
 from pathlib import Path
@@ -12,80 +12,81 @@ with open(os.path.join(app_dir, 'rsc', 'species_ids.txt')) as f:
     for l in f:
         known_species_ids.add(int(l))
 
-@app.route('/upload_sc', methods=['POST'])
+@app.route('/api/upload_sc', methods=['POST'])
 def upload_sc():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return 'No file part in the request.', 400
-    file = request.files['file']
-    # if user does not select file, file variable will be empty
-    if file.filename == '':
-        return 'No selected file.', 400
-    # generate unique session id if not already done
-    if 'uuid' not in session:
-        session['uuid'] = str(uuid.uuid4())
-    # create user directory based on session id if not exist
     
-    if file:
-        user_directory = os.path.join(app_dir, 'user_upload', session['uuid'])
-        os.makedirs(user_directory, exist_ok=True)
+    # generate unique session id if not already done
+    if 'session_id' not in session:
+        session.permanent = True
+        session['session_id'] = str(uuid.uuid4())
+    
+    # if user does not select file, file variable will be empty
+    for fname in request.files: ## only get one file
+        # print(fname)
+        file = request.files.get(fname)
+        # create user directory based on session id if not exist
+        if file:
+            
+            user_directory = os.path.join(app_dir, 'user_upload', session['session_id'], 'in')
+            os.makedirs(user_directory, exist_ok=True)
 
-        filename = 'user.sc'
-        file_path = os.path.join(user_directory,filename)
-        file.save(file_path)
+            file_path = os.path.join(user_directory, secure_filename(fname))
+            file.save(file_path)
 
-        with open(file_path, 'r') as f:
-            for i, l in enumerate(f):
-                if len(l.split('\t')) != 2:
-                    return 'File needs to be in 2-column tab-delimited format', 400
-                ll = l.split('\t')
-                if not all([i in aminoAcids for i in ll[0]]):
-                    return f'Line {i+1}: Invalid amino acid', 400
+            with open(file_path, 'r') as f:
                 try:
-                    float(ll[1])
-                except ValueError:
-                    return f'Line {i+1}: Invalid quantity value', 400
-                
-        return 'File saved successfully.', 200
+                    for i, l in enumerate(f):
+                        if len(l.split('\t')) != 2:
+                            return 'File needs to be in 2-column tab-delimited format', 400
+                        ll = l.split('\t')
+                        if not all([i in aminoAcids for i in ll[0]]):
+                            return f'Line {i+1}: Invalid amino acid', 400
+                        try:
+                            float(ll[1])
+                        except ValueError:
+                            return f'Line {i+1}: Invalid quantity value', 400
+                except:
+                    return 'Unknown file type', 400
+                    
+            return 'File saved successfully.', 200
+            
 
-@app.route('/upload_fasta', methods=['POST'])
+@app.route('/api/upload_fa', methods=['POST'])
 def upload_fasta():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return 'No file part in the request.', 400
-    file = request.files['file']
-    # if user does not select file, file variable will be empty
-    if file.filename == '':
-        return 'No selected file.', 400
     # generate unique session id if not already done
-    if 'uuid' not in session:
-        session['uuid'] = str(uuid.uuid4())
-    
-    if file:
-        user_directory = os.path.join(app_dir, 'user_upload', session['uuid'])
-        os.makedirs(user_directory, exist_ok=True)
+    if 'session_id' not in session:
+        session.permanent = True
+        session['session_id'] = str(uuid.uuid4())
 
-        filename = 'user.fa'
-        file_path = os.path.join(user_directory,filename)
+    for fname in request.files: ## only get one file
+        # print(fname)
+        file = request.files.get(fname)
+        if file:
+            user_directory = os.path.join(app_dir, 'user_upload', session['session_id'])
+            os.makedirs(user_directory, exist_ok=True)
 
-        if os.path.islink(file_path) or os.path.isfile(file_path):
-            os.remove(file_path)
+            filename = 'user.fa'
+            file_path = os.path.join(user_directory,filename)
+
+            if os.path.islink(file_path) or os.path.isfile(file_path):
+                os.remove(file_path)
 
 
-        file.save(file_path)
-        return 'File saved successfully.', 200
-    else:
-        return 'Upload unsuccessful', 400
+            file.save(file_path)
+            return 'File saved successfully.', 200
+        else:
+            return 'Upload unsuccessful', 400
 
-@app.route('/spid_check', methods = ['POST'])
+@app.route('/api/spid_check', methods = ['POST'])
 def check_id():
-    if 'uuid' not in session:
-        session['uuid'] = str(uuid.uuid4())
+    if 'session_id' not in session:
+        session.permanent = True
+        session['session_id'] = str(uuid.uuid4())
     
     spid = request.form.get('spid')
     
     if int(spid) in known_species_ids:
-        user_directory = os.path.join(app_dir, 'user_upload', session['uuid'])
+        user_directory = os.path.join(app_dir, 'user_upload', session['session_id'])
         os.makedirs(user_directory, exist_ok=True)
     
         ## download fasta file if not already exists in rsc/fasta/
@@ -102,24 +103,28 @@ def check_id():
             os.remove(file_path)
 
         Path(file_path).symlink_to(Path(f"{app_dir}/rsc/fasta/{spid}.protein.sequences.v11.5.fa").resolve())
-        return "Found fasta file can compute now"
+        return "Found fasta file can compute now", 200
     else:
-        return "Please upload custom fasta file"
+        return "Please upload custom fasta file", 400
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/api/submit', methods=['POST'])
 def compute():
-    user_directory = os.path.join(app_dir, 'user_upload', session['uuid'])
     
-    p = sbp.run(f"python3 {app_dir}/scripts/ComputeAbundanceswithSC.py -s {user_directory}/user.sc -f {user_directory}/user.fa > {user_directory}/user.out", 
-            shell=True, capture_output=True)
+    user_directory = os.path.join(app_dir, 'user_upload', session['session_id'])
     
-    if p.returncode == 0:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        return send_file(f'{base_dir}/{user_directory}/user.out', as_attachment=True)
-    else:
-        if p.stderr:
-            return p.stderr.decode('utf-8')
+    for fname in os.listdir(f'{user_directory}/in'): ## should be one file only
+        fname_out = os.path.splitext(fname)[0]+'.abu'
+        p = sbp.run(f"python3 {app_dir}/scripts/ComputeAbundanceswithSC.py -s {user_directory}/in/{fname} -f {user_directory}/user.fa > {user_directory}/{fname_out}", 
+                shell=True, capture_output=True)
+        
+        if p.returncode == 0:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            
+            return send_file(f'{base_dir}/{user_directory}/{fname_out}', as_attachment=True)
+        else:
+            if p.stderr:
+                return p.stderr.decode('utf-8')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=os.getenv('PORT'), debug=True)
